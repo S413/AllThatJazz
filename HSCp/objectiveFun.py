c9b75fb827b20d7efa678d6b9e1e6acdb2ac2007
S413 <sergioalejandrohn@hotmail.com>
2021-12-29 20:58:26 +0800

    changed file obje_fun.py to objectiveFun.py; setup took form; not sure on this thing's speed yet; haven't tested passing data from hm to this objective function either.

---
+++ HSCp/objectiveFun.py
@@ -0,0 +1,41 @@
+import numpy as np
+from sympy import symbols, Eq, solve
+import HSCp as harsea
+
+class ObjectiveFun(object):
+    """
+    Blue print class for the  Objective Function you want to employ on HS.
+
+    ...
+
+    Define a cost function.
+    Create or remove symbols for coefficients as required.
+    Use those symbols to create your function.
+    __init__ should be all you need to modify.
+    Follow the example.
+
+    ...
+
+    You can also choose to bypass sympy.
+    Modify the code to your needs.
+
+    """
+
+    def __init__(self):
+        a, b, c, d, e, f = symbols('a b c d e f')
+        self.symbols = [a,b,c,d]
+        
+        expr = 5*a**23 - 100*b**12 + 33*c**5 - e**3 + f
+        self.expr = expr
+
+    def evaluate(self, vals):
+        subst = [(self.symbols[i], vals[i]) for i in range(len(vals))]
+        res = self.expr(subst)
+        
+        return res
+        
+    def print_expr():
+    	print(self.expr)
+    	
+
+
